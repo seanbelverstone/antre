7a7af66db7eee51accf0374854957448690941c5
Sean Belverstone <seanbelverstone@gmail.com>
2025-05-07 13:56:21 -0500

    Converted the storylines.json to be a js object instead. Removed all of the modifier: none to just return an empty array instead. Updated a bunch of useless quotations

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Typewriter from 'typewriter-effect';
-import storylines from '../utils/storylines.json';
+import storylines from '../utils/storylines.js';
 
 import './css/Play.css'
 import { flattenToSingleKeys } from '../utils/functions';
@@ -9,24 +9,38 @@ import { flattenToSingleKeys } from '../utils/functions';
 const Play = () => {
 	// const { supabase } = props;
 	const character = useSelector(state => state.character);
+
 	const [storyText, setStoryText] = useState('');
 	const [typewriterDelay, setTypewriterDelay] = useState(20);
+	const [currentLevel, setCurrentLevel] = useState('');
+	// Get character level on page load
+	// create pastLevels array
+	// store current level in state
+	// if the story object's modifier is fight, show combat buttons, otherwise show Story buttons
+	// Create save button that sends update to Supabase
+	
+	useEffect(() => {
+		setCurrentLevel(storylines[character.level])
+	}, [character.level])
 
 	useEffect(() => {
 		const characterFlattened = flattenToSingleKeys(character)
-
-		const replacePlaceholders = (template, data) => {
-			return template.replace(/\{\{(.*?)\}\}/g, (_, key) => {
-				const trimmedKey = key.trim();
-				return Object.prototype.hasOwnProperty.call(data, trimmedKey)
-					? data[trimmedKey]
-					: '';
-			});
+		// Replaces words in storylines.json that are wrapped in double
+		// curly braces, like {{weapon}} for dynamic text
+		console.log(character.level);
+		console.log(storylines[`${character.level}`]);
+		if (currentLevel !== '') {
+			const replacePlaceholders = (template, data) => {
+				return template.replace(/\{\{(.*?)\}\}/g, (_, key) => {
+					const trimmedKey = key.trim();
+					return Object.prototype.hasOwnProperty.call(data, trimmedKey)
+						? data[trimmedKey]
+						: '';
+				});
+			}
+			setStoryText(replacePlaceholders(currentLevel?.text, characterFlattened));
 		}
-
-		setStoryText(replacePlaceholders(storylines[0].text, characterFlattened));
-	}, [character])
-
+	}, [character, currentLevel])
 
 	return (
 		<div id="playPage" className="page">
