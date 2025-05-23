80b664d305f75192ead101225bd75426f1ea6c00
Sean Belverstone <seanbelverstone@gmail.com>
2025-05-22 23:38:34 -0500

    Navigate wasn't defined on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom';
 import Start from './pages/Start';
 import Play from './pages/Play';
 import CharacterSelect from './pages/CharacterSelect';
